738d0cacbaf3390edf378db8a2c10747df8ca811
Benjamin Adamson <adamson.benjamin@gmail.com>
2016-06-23 18:16:52 -0700

    Pressing escape closes.

--- main.cxx
+++ main.cxx
@@ -3,25 +3,25 @@
 
 int notmain()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+      window.clear();
+      window.draw(shape);
+      window.display();
     }
-
-    return 0;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+      return 0;
+    }
+  }
+  return 0;
 }
 
 int
