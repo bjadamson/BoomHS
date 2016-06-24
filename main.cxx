6d62e88162e8b57130a87eadd2a62453efbf70d9
Benjamin Adamson <adamson.benjamin@gmail.com>
2016-06-23 17:31:57 -0700

    Setup sfml basic example.

--- main.cxx
+++ main.cxx
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+int notmain()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
 
 int
 main(int argc, char **argv)
 {
   std::cerr << "saa" << std::endl;
-  return 0;
+  return notmain();
 }
+
--- scripts/bootstrap.bash
+++ scripts/bootstrap.bash
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-set -ex
 source "scripts/common.bash"
 
 # Control Begins Here !!
-unlink ./bb.bash
-unlink ./bbc.bash
-unlink ./bbr.bash
 rm -rf ${BUILD}
 rm -f "CMakeLists.txt"
 
@@ -20,7 +16,13 @@ include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()
 
 add_executable(boomhs main.cxx)
+
+# Detect and add SFML
+set(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake_modules" ${CMAKE_MODULE_PATH})
+find_package(SFML 2.0 REQUIRED system window graphics network audio)
 target_link_libraries(boomhs ${CONAN_LIBS})
+target_link_libraries(boomhs ${SFML_LIBRARIES})
+
 EOF
 
 cat > "${BUILD}/conanfile.txt" << "EOF"
@@ -30,11 +32,11 @@ EOF
 
 cd ${BUILD}
 echo $(pwd)
-conan install -s compiler=clang -s arch=x86 -s compiler.version=3.8
+conan install -s compiler=clang -s arch=x86 -s compiler.version=3.9
 cmake .. -G "Unix Makefiles" \
   -DCMAKE_BUILD_TYPE=Debug \
-  -DCMAKE_C_COMPILER=/usr/bin/clang \
-  -DCMAKE_CXX_COMPILER=/usr/bin/clang++
+  -DCMAKE_C_COMPILER=/usr/local/bin/clang \
+  -DCMAKE_CXX_COMPILER=/usr/local/bin/clang++
 cd ..
 
 # Usage is "bb", "bbc", and "bbr"
