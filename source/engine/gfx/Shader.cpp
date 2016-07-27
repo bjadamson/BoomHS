d540cb10eb8c553511ea5ab780f40ea0b399c366
Benjamin Adamson <adamson.benjamin@gmail.com>
2016-07-26 20:04:28 -0700

    Some refactoring

--- source/engine/gfx/Shader.cpp
+++ source/engine/gfx/Shader.cpp
@@ -9,13 +9,13 @@
 #include <sstream>
 
 #include <engine/gfx/Shader.hpp>
-using namespace std;
 
 namespace
 {
 
+// TODO: move to util function
 boost::expected<std::string, std::string>
-load_shader_from_cstring_path(char const* path)
+read_file(char const* path)
 {
   // Read the Vertex Shader code from the file
   std::ifstream istream(path, std::ios::in);
@@ -34,6 +34,23 @@ load_shader_from_cstring_path(char const* path)
   return sstream.str();
 }
 
+inline bool
+is_compiled(GLuint const handle)
+{
+  GLint is_compiled = GL_FALSE;
+  glGetShaderiv(handle, GL_COMPILE_STATUS, &is_compiled);
+  return GL_FALSE != is_compiled;
+}
+
+inline void
+gl_compile_shader(GLuint const handle, char const* source)
+{
+  GLint *p_length = nullptr;
+  auto constexpr shader_count = 1; // We're only compiling one shader (in this function anyway).
+  glShaderSource(handle, 1, &source, p_length);
+  glCompileShader(handle);
+}
+
 } // ns anonymous
 
 namespace engine
@@ -41,10 +58,16 @@ namespace engine
 namespace gfx
 {
 
-boost::expected<GLuint, std::string>
-compile_shader(char const* data, GLenum const type)
+// TODO: export as reusable fn
+// also, make an abstraction over the source, not just vector<char>
+inline std::string
+retrieve_shader_log(GLuint const handle)
 {
-  GLuint const handle = glCreateShader(type);
+  // We have to do a low-level dance to get the OpenGL shader logs.
+  //
+  // 1. Ask OpenGL how buffer space we need to retrieve the buffer.
+  // 2. Retrieve the data into our buffer.
+  // 3. Return the buffer.
 
   // TODO: move this into std::utils, something like std::vec::with_size() (also with_capacity())
   auto vec_with_size = [](auto const size) {
@@ -53,27 +76,34 @@ compile_shader(char const* data, GLenum const type)
     return buffer;
   };
 
-  // Compile Vertex Shader
-  glShaderSource(handle, 1, &data, nullptr);
-  glCompileShader(handle);
-
-  // Check Vertex Shader
-  GLint is_compiled = GL_FALSE;
-  glGetShaderiv(handle, GL_COMPILE_STATUS, &is_compiled);
-  if (is_compiled) {
-    return handle;
-  }
-  GLint InfoLogLength = 0;
-  glGetShaderiv(handle, GL_INFO_LOG_LENGTH, &InfoLogLength);
-  if (0 > InfoLogLength) {
-    auto constexpr err = "Compiling shader failed and could not retrieve OpenGL Log info for object";
-    return boost::make_unexpected(err);
+  // Step 1
+  GLint log_length = 0;
+  glGetShaderiv(handle, GL_INFO_LOG_LENGTH, &log_length);
+  if (0 > log_length) {
+    // fix this msg
+    return "Compiling shader failed and could not retrieve OpenGL Log info for object";
   }
 
-  auto const buffer_size = InfoLogLength + 1; // +1 for null terminator character '\0'
+  // Step 2
+  auto const buffer_size = log_length + 1; // +1 for null terminator character '\0'
   auto buffer = vec_with_size(buffer_size);
   glGetShaderInfoLog(handle, buffer_size, nullptr, buffer.data());
-  return boost::make_unexpected( std::string{buffer.cbegin(), buffer.cend()} );
+
+  // Step 3
+  return std::string{buffer.cbegin(), buffer.cend()};
+}
+
+boost::expected<GLuint, std::string>
+compile_shader(char const* data, GLenum const type)
+{
+  GLuint const handle = glCreateShader(type);
+  gl_compile_shader(handle, data);
+
+  // Check Vertex Shader
+  if (true == is_compiled(handle)) {
+    return handle;
+  }
+  return boost::make_unexpected(retrieve_shader_log(handle));
 }
 
 template<typename T>
@@ -99,10 +129,10 @@ LoadShaders(char const* vertex_file_path, char const* fragment_file_path)
 #define DO_MONAD(VAR_DECL, expr) ONLY_IFOK_HELPER(VAR_DECL, __COUNTER__, expr)
 
   // Read the Vertex Shader code from the file
-  DO_MONAD(auto vertex_shader_source, load_shader_from_cstring_path(vertex_file_path));
+  DO_MONAD(auto vertex_shader_source, read_file(vertex_file_path));
 
   // Read the Fragment Shader code from the file
-  DO_MONAD(auto fragment_shader_source, load_shader_from_cstring_path(fragment_file_path));
+  DO_MONAD(auto fragment_shader_source, read_file(fragment_file_path));
 
   DO_MONAD(GLuint const VertexShaderID, compile_shader(vertex_shader_source, GL_VERTEX_SHADER));
   DO_MONAD(GLuint const FragmentShaderID, compile_shader(fragment_shader_source, GL_FRAGMENT_SHADER));
