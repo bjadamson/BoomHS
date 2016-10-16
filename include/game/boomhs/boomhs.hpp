b07b9b2d247db352871864eb391ff3c31b221c5b
Benjamin Adamson <adamson.benjamin@gmail.com>
2016-10-16 01:06:03 -0700

    Move io code out of boomhs

--- include/game/boomhs/boomhs.hpp
+++ include/game/boomhs/boomhs.hpp
@@ -12,11 +12,16 @@
 #include <stlw/result.hpp>
 #include <stlw/type_ctors.hpp>
 
+#include <game/boomhs/io_system.hpp>
 #include <game/data_types.hpp>
 
-namespace game
-{
-namespace boomhs
+namespace st {
+
+constexpr auto io_system = ecst::tag::system::v<s::io_system>;
+
+} // ns st
+
+namespace game::boomhs
 {
 
 // Define some components.
@@ -29,120 +34,6 @@ namespace ct
 // Define component tags.
 namespace sc = ecst::signature::component;
 } // ns ct
-
-// Define some systems.
-namespace s
-{
-
-struct io_system
-{
-  inline static bool is_quit_event(SDL_Event &event)
-  {
-    bool is_quit = false;
-
-    switch (event.type) {
-    case SDL_QUIT: {
-      is_quit = true;
-      break;
-    }
-    case SDL_KEYDOWN: {
-      switch (event.key.keysym.sym) {
-      case SDLK_ESCAPE: {
-        is_quit = true;
-        break;
-      }
-      }
-    }
-    }
-    return is_quit;
-  }
-  bool process_event(SDL_Event &event, glm::mat4 &view, glm::mat4 &proj) const
-  {
-    float constexpr DISTANCE = 0.1f;
-    float constexpr ANGLE = 0.2f;
-    float constexpr SCALE_FACTOR = 0.1f;
-
-    auto const make_scalev = [](float const f) { return glm::vec3(1.0f + f, 1.0f + f, 1.0f + f); };
-
-    switch (event.type) {
-    case SDL_KEYDOWN: {
-      switch (event.key.keysym.sym) {
-      // translation
-      case SDLK_w: {
-        view = glm::translate(view, glm::vec3(0.0f, DISTANCE, 0.0f));
-        break;
-      }
-      case SDLK_s: {
-        view = glm::translate(view, glm::vec3(0.0f, -DISTANCE, 0.0f));
-        break;
-      }
-      case SDLK_a: {
-        view = glm::translate(view, glm::vec3(-DISTANCE, 0.0f, 0.0f));
-        break;
-      }
-      case SDLK_d: {
-        view = glm::translate(view, glm::vec3(DISTANCE, 0.0f, 0.0f));
-        break;
-      }
-      // scaling
-      case SDLK_o: {
-        view = glm::scale(view, make_scalev(SCALE_FACTOR));
-        break;
-      }
-      case SDLK_p: {
-        view = glm::scale(view, make_scalev(-SCALE_FACTOR));
-        break;
-      }
-      // z-rotation
-      case SDLK_j: {
-        view = glm::rotate(view, ANGLE, glm::vec3(0.0f, 0.0f, 1.0f));
-        break;
-      }
-      case SDLK_k: {
-        view = glm::rotate(view, -ANGLE, glm::vec3(0.0f, 0.0f, 1.0f));
-        break;
-      }
-      // y-rotation
-      case SDLK_u: {
-        view = glm::rotate(view, ANGLE, glm::vec3(0.0f, 1.0f, 0.0f));
-        break;
-      }
-      case SDLK_i: {
-        view = glm::rotate(view, -ANGLE, glm::vec3(0.0f, 1.0f, 0.0f));
-        break;
-      }
-      // x-rotation
-      case SDLK_n: {
-        view = glm::rotate(view, ANGLE, glm::vec3(1.0f, 0.0f, 0.0f));
-        break;
-      }
-      case SDLK_m: {
-        view = glm::rotate(view, -ANGLE, glm::vec3(1.0f, 0.0f, 0.0f));
-        break;
-      }
-      }
-    }
-    }
-    return is_quit_event(event);
-  }
-
-  template <typename TData, typename S>
-  void process(TData &data, S &state)
-  {
-    SDL_Event event;
-    while ((! state.quit) && (0 != SDL_PollEvent(&event))) {
-      state.quit = this->process_event(event, state.view, state.projection);
-    }
-  }
-};
-
-} // ns s
-
-namespace st {
-
-constexpr auto io_system = ecst::tag::system::v<s::io_system>;
-
-} // ns st
 
 // Setup compile-time settings.
 namespace ecst_setup
@@ -207,8 +98,8 @@ ecst_main(G &game, S &state)
 
   // Define ECST context settings.
   constexpr auto s = ecst::settings::make()
-                        //.allow_inner_parallelism()
-                        .disallow_inner_parallelism()
+                        .allow_inner_parallelism()
+                        //.disallow_inner_parallelism()
                         .fixed_entity_limit(ecst::sz_v<10000>)
                         .component_signatures(make_csl())
                         .system_signatures(make_ssl())
@@ -278,5 +169,4 @@ public:
   }
 };
 
-} // ns boomhs
-} // ns game
+} // ns game::boomhs
---
+++ include/game/boomhs/io_system.hpp
@@ -0,0 +1,114 @@
+#pragma once
+#include <ecst.hpp>
+
+// Define some systems.
+
+
+
+namespace s
+{
+
+struct io_system
+{
+  inline static bool is_quit_event(SDL_Event &event)
+  {
+    bool is_quit = false;
+
+    switch (event.type) {
+    case SDL_QUIT: {
+      is_quit = true;
+      break;
+    }
+    case SDL_KEYDOWN: {
+      switch (event.key.keysym.sym) {
+      case SDLK_ESCAPE: {
+        is_quit = true;
+        break;
+      }
+      }
+    }
+    }
+    return is_quit;
+  }
+  bool process_event(SDL_Event &event, glm::mat4 &view, glm::mat4 &proj) const
+  {
+    float constexpr DISTANCE = 0.1f;
+    float constexpr ANGLE = 0.2f;
+    float constexpr SCALE_FACTOR = 0.1f;
+
+    auto const make_scalev = [](float const f) { return glm::vec3(1.0f + f, 1.0f + f, 1.0f + f); };
+
+    switch (event.type) {
+    case SDL_KEYDOWN: {
+      switch (event.key.keysym.sym) {
+      // translation
+      case SDLK_w: {
+        view = glm::translate(view, glm::vec3(0.0f, DISTANCE, 0.0f));
+        break;
+      }
+      case SDLK_s: {
+        view = glm::translate(view, glm::vec3(0.0f, -DISTANCE, 0.0f));
+        break;
+      }
+      case SDLK_a: {
+        view = glm::translate(view, glm::vec3(-DISTANCE, 0.0f, 0.0f));
+        break;
+      }
+      case SDLK_d: {
+        view = glm::translate(view, glm::vec3(DISTANCE, 0.0f, 0.0f));
+        break;
+      }
+      // scaling
+      case SDLK_o: {
+        view = glm::scale(view, make_scalev(SCALE_FACTOR));
+        break;
+      }
+      case SDLK_p: {
+        view = glm::scale(view, make_scalev(-SCALE_FACTOR));
+        break;
+      }
+      // z-rotation
+      case SDLK_j: {
+        view = glm::rotate(view, ANGLE, glm::vec3(0.0f, 0.0f, 1.0f));
+        break;
+      }
+      case SDLK_k: {
+        view = glm::rotate(view, -ANGLE, glm::vec3(0.0f, 0.0f, 1.0f));
+        break;
+      }
+      // y-rotation
+      case SDLK_u: {
+        view = glm::rotate(view, ANGLE, glm::vec3(0.0f, 1.0f, 0.0f));
+        break;
+      }
+      case SDLK_i: {
+        view = glm::rotate(view, -ANGLE, glm::vec3(0.0f, 1.0f, 0.0f));
+        break;
+      }
+      // x-rotation
+      case SDLK_n: {
+        view = glm::rotate(view, ANGLE, glm::vec3(1.0f, 0.0f, 0.0f));
+        break;
+      }
+      case SDLK_m: {
+        view = glm::rotate(view, -ANGLE, glm::vec3(1.0f, 0.0f, 0.0f));
+        break;
+      }
+      }
+    }
+    }
+    return is_quit_event(event);
+  }
+
+  template <typename TData, typename S>
+  void process(TData &data, S &state)
+  {
+    SDL_Event event;
+    while ((! state.quit) && (0 != SDL_PollEvent(&event))) {
+      state.quit = this->process_event(event, state.view, state.projection);
+    }
+  }
+};
+
+} // ns s
+
