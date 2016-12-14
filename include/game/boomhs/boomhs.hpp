c7311e085768581ef2e7399c7d6a18a22426811f
Benjamin Adamson <adamson.benjamin@gmail.com>
2016-12-13 22:18:57 -0800

    Fixed the 3d rotation, finally figured out what I was doing wrong. 2d objects need to be fixed and wc removed before merging to release.

--- include/game/boomhs/boomhs.hpp
+++ include/game/boomhs/boomhs.hpp
@@ -112,6 +112,7 @@ ecst_main(G &game, S &state)
 
   // Initialize context with some entities.
   ctx->step([&](auto &proxy) {
+    /*
     {
       auto eid = proxy.create_entity();
       auto &wc = proxy.add_component(ct::world_coordinate, eid);
@@ -182,26 +183,29 @@ ecst_main(G &game, S &state)
       y = &wc;
       ymv = &proxy.add_component(ct::model, eid);
     }
+    */
     {
       auto eid = proxy.create_entity();
       auto &wc = proxy.add_component(ct::world_coordinate, eid);
-      wc.set(0.0f, 0.0f, 0.0f, 1.0f);
       a = &wc;
       amv = &proxy.add_component(ct::model, eid);
     }
     {
       auto eid = proxy.create_entity();
       auto &wc = proxy.add_component(ct::world_coordinate, eid);
-      wc.set(-0.7f, 0.7f, 0.0f, 1.0f);
+      //wc.set(-0.7f, 0.7f, 0.0f, 1.0f);
       b = &wc;
       bmv = &proxy.add_component(ct::model, eid);
+      bmv->translation += glm::vec3{-0.7f, 0.0f, 0.0f};
     }
+    /*
     {
       auto eid = proxy.create_entity();
       auto &wc = proxy.add_component(ct::world_coordinate, eid);
       wc.set(0.7f, -0.7f, 0.0f, 1.0f);
       c = &wc;
     }
+    */
   });
 
   namespace sea = ecst::system_execution_adapter;
@@ -269,25 +273,25 @@ public:
                                       stlw::concat(engine::gfx::LIST_OF_COLORS::YELLOW, 1.0f));
 
     auto const height = 0.25f, width = 0.39f;
-    auto triangle_color = game::triangle_factory::make(*p, ::engine::gfx::LIST_OF_COLORS::PINK);
-    auto triangle_list_colors = game::triangle_factory::make(*q, multicolor_triangle);
-    auto triangle_texture = game::triangle_factory::make(*r, true);
-    auto triangle_wireframe = game::triangle_factory::make(*s, true, false);
+    //auto triangle_color = game::triangle_factory::make(*p, ::engine::gfx::LIST_OF_COLORS::PINK);
+    //auto triangle_list_colors = game::triangle_factory::make(*q, multicolor_triangle);
+    //auto triangle_texture = game::triangle_factory::make(*r, true);
+    //auto triangle_wireframe = game::triangle_factory::make(*s, true, false);
 
     auto cube_texture = game::cube_factory::make_textured(*a, *amv, {0.25f, 0.25f, 0.25f});
     auto cube_color = game::cube_factory::make_spotted(*b, *bmv, ::engine::gfx::LIST_OF_COLORS::BLUE,
         {0.25f, 0.25f, 0.25f});
     //auto cube_wf = game::cube_factory::make_wireframe(*c, *cmv, {0.25f, 0.25f, 0.25f});
 
-    auto rectangle_color = game::rectangle_factory::make(*t, ::engine::gfx::LIST_OF_COLORS::YELLOW);
-    auto rectangle_list_colors = game::rectangle_factory::make(*u, height, width, multicolor_rect);
-    auto rectangle_texture = game::rectangle_factory::make(*v, height, width, true);
+    //auto rectangle_color = game::rectangle_factory::make(*t, ::engine::gfx::LIST_OF_COLORS::YELLOW);
+    //auto rectangle_list_colors = game::rectangle_factory::make(*u, height, width, multicolor_rect);
+    //auto rectangle_texture = game::rectangle_factory::make(*v, height, width, true);
     //auto rectangle_wireframe = game::rectangle_factory::make(*w, height, width, true, true);
 
-    auto polygon_color =
-        game::polygon_factory::make(*x, 5, ::engine::gfx::LIST_OF_COLORS::DARK_ORANGE);
+    //auto polygon_color =
+        //game::polygon_factory::make(*x, 5, ::engine::gfx::LIST_OF_COLORS::DARK_ORANGE);
     // auto polygon_list_of_color = game::polygon_factory::make(*u, 5, multicolor_triangle);
-    auto polygon_texture = game::polygon_factory::make(*y, 7, true);
+    //auto polygon_texture = game::polygon_factory::make(*y, 7, true);
     //auto polygon_wireframe = game::polygon_factory::make(*v, 7, true, true);
 
     state.renderer.begin();
