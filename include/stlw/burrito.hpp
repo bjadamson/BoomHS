a1fd506358227b5e939570342853c29b6b065c89
Benjamin Adamson <adamson.benjamin@gmail.com>
2016-12-20 05:56:16 -0800

    remove dead code from merge

--- include/stlw/burrito.hpp
+++ include/stlw/burrito.hpp
@@ -105,38 +105,6 @@ auto map(burrito<U, container_tag> const& b, FN const& fn)
   return detail::map_impl<T>(b.value, fn);
 }
 
-/*
-template<typename U, typename FN, template <class, class> typename Container>
-auto constexpr map(burrito<U, container_tag> const& b, FN const& fn,
-    Container<typename std::decay<decltype(*b.value.first)>::type,
-    std::allocator<typename std::decay<decltype(*b.value.first)>::type>> &&cont)
-{
-  using T1 = decltype(b.value.first);
-  using T2 = decltype(b.value.second);
-  static_assert(std::is_same<T1, T2>(), "iterator pairs must be of the same type.");
-
-  for (auto it{b.value.first}; it < b.value.second; ++it) {
-    cont.emplace_back(fn(*it));
-  }
-  return cont;
-}
-
-template<typename U, typename FN, template <class, std::size_t> typename Container, std::size_t N>
-auto constexpr map(burrito<U, container_tag> const& b, FN const& fn,
-    Container<typename std::decay<decltype(*b.value.first)>::type, N> &&cont)
-{
-  using T1 = decltype(b.value.first);
-  using T2 = decltype(b.value.second);
-  static_assert(std::is_same<T1, T2>(), "iterator pairs must be of the same type.");
-
-  auto i{0};
-  for (auto it{b.value.first}; it < b.value.second; ++it) {
-    cont[i++] = fn(*it);
-  }
-  return std::move(cont);
-}
-*/
-
 template<typename U, typename FN>
 void constexpr for_each(burrito<U, tuple_tag> const& b, FN const& fn)
 {
