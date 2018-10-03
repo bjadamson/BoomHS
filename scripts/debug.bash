d8340e08a936b001ba77c476867339f424acac45
Benjamin Adamson <adamson.benjamin@gmail.com>
2018-10-03 13:20:55 -0700

    Have debug script work with static analysis

--- scripts/debug.bash
+++ scripts/debug.bash
@@ -2,4 +2,16 @@
 set -ex
 source "scripts/common.bash"
 
-gdb ${BUILD}/bin/boomhs
+
+
+source "scripts/common.bash"
+
+# Allow core dumps
+ulimit -c unlimited
+
+# Address Sanitizer Runtime Flags
+ADDRESS_SANITIZER_FLAGS="verbosity=1:detect_container_overflow=0:check_initialization_order=1:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1"
+LEAK_DETECTOR_FLAGS="suppressions=external/llvm_leaks.supp"
+
+DEBUG_PROGRAM="gdb ${BUILD}/bin/boomhs"
+ASAN_OPTIONS=${ADDRESS_SANITIZER_FLAGS} LSAN_OPTIONS=${LEAK_DETECTOR_FLAGS} ${DEBUG_PROGRAM}
