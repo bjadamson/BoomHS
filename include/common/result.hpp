8e1d75b7fc386586b416d570248a67b7d0c8a479
Benjamin Adamson <adamson.benjamin@gmail.com>
2018-11-29 09:11:04 -0800

    work towards macros

--- include/common/result.hpp
+++ include/common/result.hpp
@@ -22,15 +22,18 @@
 
 //
 // HELPER MACRO
-#define DO_EFFECT_CONCAT(VAR_NAME, expr) DO_EFFECT_EVAL(_DO_EFFECT_TEMPORARY_##VAR_NAME, expr)
+#define DO_EFFECT_CONCAT(VAR_NAME, expr)                                                           \
+  DO_EFFECT_EVAL(_DO_EFFECT_TEMPORARY_##VAR_NAME, expr)
 
 //
 // HELPER MACRO
-#define DO_EFFECT_EXPAND_VAR(VAR_NAME, expr) DO_EFFECT_CONCAT(VAR_NAME, expr)
+#define DO_EFFECT_EXPAND_VAR(VAR_NAME, expr)                                                       \
+  DO_EFFECT_CONCAT(VAR_NAME, expr)
 
 //
 // DO_EFFECT MACRO
-#define DO_EFFECT(expr) DO_EFFECT_EXPAND_VAR(__COUNTER__, expr)
+#define DO_EFFECT(expr)                                                                            \
+  DO_EFFECT_EXPAND_VAR(__COUNTER__, expr)
 
 // OK_MOVE
 //
@@ -61,11 +64,28 @@
 //   The value returned from the function invocation is returned at the callsite of the
 //   macro.
 //   * Otherwise MOVES the evaluated expression into the variable VAR_NAME.
-#define TRY_OR(VAR_NAME, expr, fn)                                                                 \
-  EVAL_INTO_VAR_OR(VAR_NAME, expr, [&](auto&& r) { return fn(r.unwrapErr()); })
+#define TRY_OR(VAR_NAME, expr, else_fn)                                                            \
+  EVAL_INTO_VAR_OR(VAR_NAME, expr, [&](auto&& r) { return else_fn(r.unwrapErr()); })
 
-#define TRY_EFFECT_OR(expr, fn)                                                          \
-  TRY_OR(auto __, expr, fn)
+//
+// HELPER MACRO
+#define TRY_EFFECT_OR_HELPER(VAR_NAME, expr, else_fn)                                              \
+  TRY_OR(auto VAR_NAME, expr, else_fn)
+
+//
+// HELPER MACRO
+#define TRY_EFFECT_OR_CONCAT(VAR_NAME, expr, else_fn)                                              \
+  TRY_EFFECT_OR_HELPER(_TRY_EFFECT_TEMPORARY_##VAR_NAME, expr, else_fn)
+
+//
+// HELPER MACRO
+#define TRY_EFFECT_OR_EXPAND_VAR(VAR_NAME, expr, else_fn)                                          \
+  TRY_EFFECT_OR_CONCAT(VAR_NAME, expr, else_fn)
+
+// Invoke the expression, if it returns an Error result, return the value of invoking the on_error
+// fn.
+#define TRY_EFFECT_OR(expr, on_errorfn)                                                  \
+  TRY_EFFECT_OR_EXPAND_VAR(__COUNTER__, expr, on_errorfn)
 
 // TRY_OR_COMPOUND_STATEMENT
 //
@@ -76,9 +96,18 @@
 // allowing cleaner usage at the macro invocatio site.
 //
 // NOTE: I am leaving TRY_OR() in this file in case there is a compiler (MSVC? not sure at the time
-// of writing this) that will not support this macro invocation.
+// of writing this) that will not support this macro invocation, due to not recognizing the
+// compounte statement compiler extension supported by both gcc/clang.
 //
-// Uses a compiler extension supported by GCC and clang.
+// If Visual Studio ever supports the compound statement extension, or it gets merged into the C++
+// standard as a language feature, rename this macro "TRY_OR" and delete the implementation above,
+// then rename this macro TRY_OR and update the usage sites to
+//
+// from:
+//   TRY_OR(auto abc, expr, ...)
+//
+// to:
+//   auto abc = TRY_OR(expr, ...);
 #define TRY_OR_COMPOUND_STATEMENT(expr, fn)                                                        \
     ({                                                                                             \
         auto res = expr;                                                                           \
@@ -90,16 +119,15 @@
         res.storage().get_moveout<T>();                                                            \
     })
 
-//
-// A dummy structure.
 namespace common
 {
 
-struct Nothing
+//
+// A dummy structure. Used to indicate "Nothing"
+struct none_t
 {
 };
-
-using none_t = Nothing;
+using Nothing = none_t;
 
 } // namespace common
 
